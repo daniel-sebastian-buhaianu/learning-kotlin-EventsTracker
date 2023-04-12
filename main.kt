54eadfd233236b736713b539ea17f871368f9f89
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-04-12 12:35:00 +0300

    refactored code and added events

--- main.kt
+++ main.kt
@@ -1,19 +1,25 @@
+fun main() {
+    // hard coded events by team members
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+    
+    // store events in a mutable list
+    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event 6)
+}
+
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
+
 // holds the details of the event entered by the user
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: String,
+    val daypart: Daypart,
     val durationInMinutes: Int
 )
 
-fun main() {
-    
-    // test code functionality
-    val event = Event(
-        "Study Kotlin",
-        "Commit to studying Kotlin at least 15 minutes per day",
-        "Evening",
-        15
-    ).toString()
-    println(event)
-}
